7c5c9601e62950041ad3ee26cd9da23665abae3f
Nick <nick-schokker@hotmail.com>
2018-10-09 15:36:49 +0200

    Added checks on fields

--- app/src/main/java/skillsvision/skillsvisionapp/activities/RegisterAccountActivity.java
+++ app/src/main/java/skillsvision/skillsvisionapp/activities/RegisterAccountActivity.java
@@ -2,19 +2,32 @@ package skillsvision.skillsvisionapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
 
 import skillsvision.skillsvisionapp.R;
 
 public class RegisterAccountActivity extends AppCompatActivity {
 
     private static Holder mHolder = new Holder();
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE);
+    public static final Pattern VALID_DATE_REGEX =
+            Pattern.compile("^\\d{2}-\\d{2}-\\d{4}$");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,121 @@ public class RegisterAccountActivity extends AppCompatActivity {
             }
         });
 
+        mHolder.mRegisterAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!checkEmptyFields()){
+                    return;
+                } else {
+                    final String emailadress = mHolder.mEmailEditText.getText().toString().trim();
+                    final String password;
+                    final String birthdate;
+                    final String location;
+
+                    if (!VALID_EMAIL_ADDRESS_REGEX.matcher(emailadress).matches()){
+                        mHolder.mEmailEditText.requestFocus();
+                        mHolder.mEmailEditText.setError("The provided emailadress is invalid");
+                        return;
+                    } else if(mHolder.mPasswordEditText.getText().toString().length() <= 4){
+                        mHolder.mPasswordEditText.requestFocus();
+                        mHolder.mPasswordEditText.setError("A password should be atleast 4 characters long. \n" +
+                                                           "A strong password has the following:\n" +
+                                                           " - Atleast 1 lowercase letter\n" +
+                                                           " - Atleast 1 uppercase letter\n" +
+                                                           " - Atleast 1 number\n" +
+                                                           " - Atleast 1 character");
+                        return;
+                    } else if (!mHolder.mPasswordEditText.getText().toString().equals(mHolder.mConfirmPasswordEditText.getText().toString())){
+                        mHolder.mConfirmPasswordEditText.requestFocus();
+                        mHolder.mPasswordEditText.setError("Passwords are not identical");
+                        mHolder.mConfirmPasswordEditText.setError("Passwords are not identical");
+                        return;
+                    } else if(!VALID_DATE_REGEX.matcher(mHolder.mBirthdateEditText.getText().toString()).matches()){
+                        mHolder.mBirthdateEditText.requestFocus();
+                        mHolder.mBirthdateEditText.setError("Field is empty or birthdate should be written as DD-MM-YYYY");
+                        return;
+                    } else {
+                        password = mHolder.mPasswordEditText.getText().toString().trim();
+                        /* Parse Date into correct format for database */
+                        DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+                        Date dateObject = null;
+                        birthdate = mHolder.mBirthdateEditText.getText().toString();
+                        try {
+                            dateObject = formatter.parse(birthdate);
+                            Toast.makeText(RegisterAccountActivity.this, dateObject.toString(), Toast.LENGTH_LONG).show();
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                        location = mHolder.mLocationEditText.getText().toString().trim();
+                    }
+                }
+            }
+        });
+    }
+
+    private Boolean checkEmptyFields(){
+        if (mHolder.mEmailEditText.getText().toString().isEmpty() &&
+                mHolder.mPasswordEditText.getText().toString().isEmpty() &&
+                mHolder.mConfirmPasswordEditText.getText().toString().isEmpty()&&
+                mHolder.mBirthdateEditText.getText().toString().isEmpty() &&
+                mHolder.mLocationEditText.getText().toString().isEmpty()){
+            mHolder.mEmailEditText.setError("Field is empty");
+            mHolder.mPasswordEditText.setError("Field is empty");
+            mHolder.mConfirmPasswordEditText.setError("Field is empty");
+            mHolder.mBirthdateEditText.setError("Field is empty");
+            mHolder.mLocationEditText.setError("Field is empty");
+            Toast.makeText(RegisterAccountActivity.this, "All fields are empty", Toast.LENGTH_LONG).show();
+            mHolder.mEmailEditText.requestFocus();
+            return false;
+        }else if (mHolder.mPasswordEditText.getText().toString().isEmpty() &&
+                mHolder.mConfirmPasswordEditText.getText().toString().isEmpty()&&
+                mHolder.mBirthdateEditText.getText().toString().isEmpty() &&
+                mHolder.mLocationEditText.getText().toString().isEmpty()){
+            mHolder.mEmailEditText.setError(null);
+            mHolder.mPasswordEditText.setError("Field is empty");
+            mHolder.mConfirmPasswordEditText.setError("Field is empty");
+            mHolder.mBirthdateEditText.setError("Field is empty");
+            mHolder.mLocationEditText.setError("Field is empty");
+            Toast.makeText(RegisterAccountActivity.this, "Some fields are empty", Toast.LENGTH_LONG).show();
+            mHolder.mPasswordEditText.requestFocus();
+            return false;
+        } else if (mHolder.mConfirmPasswordEditText.getText().toString().isEmpty()&&
+                mHolder.mBirthdateEditText.getText().toString().isEmpty() &&
+                mHolder.mLocationEditText.getText().toString().isEmpty()){
+            mHolder.mConfirmPasswordEditText.setError("Field is empty");
+            mHolder.mBirthdateEditText.setError("Field is empty");
+            mHolder.mLocationEditText.setError("Field is empty");
+            mHolder.mEmailEditText.setError(null);
+            mHolder.mPasswordEditText.setError(null);
+            Toast.makeText(RegisterAccountActivity.this, "Some fields are empty", Toast.LENGTH_LONG).show();
+            mHolder.mConfirmPasswordEditText.requestFocus();
+            return false;
+        }else if (mHolder.mBirthdateEditText.getText().toString().isEmpty() &&
+                mHolder.mLocationEditText.getText().toString().isEmpty()){
+            mHolder.mBirthdateEditText.setError("Field is empty");
+            mHolder.mLocationEditText.setError("Field is empty");
+            mHolder.mEmailEditText.setError(null);
+            mHolder.mPasswordEditText.setError(null);
+            mHolder.mConfirmPasswordEditText.setError(null);
+            Toast.makeText(RegisterAccountActivity.this, "Some fields are empty", Toast.LENGTH_LONG).show();
+            mHolder.mBirthdateEditText.requestFocus();
+            return false;
+        }else if (mHolder.mLocationEditText.getText().toString().isEmpty()){
+            mHolder.mLocationEditText.setError("Field is empty");
+            mHolder.mEmailEditText.setError(null);
+            mHolder.mPasswordEditText.setError(null);
+            mHolder.mConfirmPasswordEditText.setError(null);
+            mHolder.mBirthdateEditText.setError(null);
+            Toast.makeText(RegisterAccountActivity.this, "A field is empty", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            mHolder.mEmailEditText.setError(null);
+            mHolder.mPasswordEditText.setError(null);
+            mHolder.mConfirmPasswordEditText.setError(null);
+            mHolder.mBirthdateEditText.setError(null);
+            mHolder.mLocationEditText.setError(null);
+            return true;
+        }
     }
 
     private void initViews(){
